db811d4cf3fc9c90b556c0275ddc77ee77f5bd7f
Nerglej <william@jelgren.dk>
2021-11-23 12:31:29 +0100

    Centered everything

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,6 +8,18 @@
 </head>
 
 <main>
-    <slot />
+    <div class="centered">
+        <slot />
+    </div>
     <NavigationBarWrapper/>
 </main>
+
+<style>
+    .centered {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+</style>
